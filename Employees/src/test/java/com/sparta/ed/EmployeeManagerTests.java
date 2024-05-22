eb8251dc7820d9b8e266d3bb66863956104567a4
Conner Humphrey <chumphrey@spartaglobal.com>
2024-05-22 15:06:17 +0100

    fixed convertStringToDate function so it properly works with edge cases and passes all previous tests

--- Employees/src/test/java/com/sparta/ed/EmployeeManagerTests.java
+++ Employees/src/test/java/com/sparta/ed/EmployeeManagerTests.java
@@ -49,7 +49,9 @@ public class EmployeeManagerTests {
     @Test
     @DisplayName("createEmployee test ensuring employee is created properly given 198429,\t\"Mrs.\",\"Serafina\", 'I', \"Bumgarner\",'F',\t\"serafina.bumgarner@exxonmobil.com\",\"9/21/1982\",\t\"2/1/2008\",69294 as input")
     void createEmployeeTestGivenAboveInput(){
-        Employee expectedEmployee = new Employee("198429","Mrs.","Serafina","I", "Bumgarner","F",	"serafina.bumgarner@exxonmobil.com","9/21/1982","2/1/2008","69294");
+        LocalDate dob = VariableParsing.convertStringToDate("9/21/1982");
+        LocalDate dateJoined = VariableParsing.convertStringToDate("2/1/2008");
+        Employee expectedEmployee = new Employee(198429,"Mrs.","Serafina","I", "Bumgarner","F",	"serafina.bumgarner@exxonmobil.com",dob,dateJoined,69294);
         Employee actualEmployee = employeeManager.createEmployee("198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,2/1/2008,69294");
 
         Assertions.assertEquals(expectedEmployee.toString(), actualEmployee.toString());
@@ -65,7 +67,9 @@ public class EmployeeManagerTests {
     @DisplayName("readEmployees - correct first employee")
     void readEmployeesCorrectFirstEmployee(){
         employees = employeeManager.readEmployees(myPath);
-        Employee expectedEmployee = new Employee(198429,"Mrs.","Serafina","I","Bumgarner","F","serafina.bumgarner@exxonmobil.com","9/21/1982","02/01/2008","69294");
+        LocalDate dob = VariableParsing.convertStringToDate("9/21/1982");
+        LocalDate dateJoined = VariableParsing.convertStringToDate("02/01/2008");
+        Employee expectedEmployee = new Employee(198429,"Mrs.","Serafina","I","Bumgarner","F","serafina.bumgarner@exxonmobil.com",dob,dateJoined,69294);
         Employee firstEmployee = employees.get(0);
         Assertions.assertEquals(expectedEmployee.toString(), firstEmployee.toString());
     }
