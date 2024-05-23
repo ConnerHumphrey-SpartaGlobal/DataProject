ca2e883f8fa705948af0269711b94d84a5c9c855
Conner Humphrey <chumphrey@spartaglobal.com>
2024-05-23 16:02:52 +0100

    5 regex methods done

--- Employees/src/main/java/com/sparta/ed/VariableValidation.java
+++ Employees/src/main/java/com/sparta/ed/VariableValidation.java
@@ -2,16 +2,49 @@ package com.sparta.ed;
 
 public class VariableValidation {
 
-    public static boolean validateID(){}
-    public static boolean validatePrefix(){}
-    public static boolean validateFirstName(){}
-    public static boolean validateInitial(){}
-    public static boolean validateLastName(){}
-    public static boolean validateGender(){}
-    public static boolean validateEMail(){}
-    public static boolean validateDob(){}
-    public static boolean validateJoiningDate(){}
-    public static boolean validateSalary(){}
+    public static boolean validateID(String empID){
+        String iDRegex = "[0-9]{6}";
+        return empID.matches(iDRegex);
+
+    }
+    public static boolean validatePrefix(String title){
+        String titleRegex = "^(?:Mr|Mrs|Ms|Dr|Mx|Prof)\\.";
+        return title.matches(titleRegex);
+
+    }
+    public static boolean validateFirstName(String firstName){
+        String firstNameRegex = "^[A-z]*";
+        return firstName.matches(firstNameRegex);
+
+    }
+    public static boolean validateInitial(String initial){
+        String initialRegex = "^[A-Z]";
+        return initial.matches(initialRegex);
+
+    }
+    public static boolean validateLastName(String lastName){
+        String lastNameRegex = "^[A-z]*";
+        return lastNameRegex.matches(lastNameRegex);
+
+    }
+    public static boolean validateGender(){
+        return false;
+
+    }
+    public static boolean validateEMail(){
+        return false;
+
+    }
+    public static boolean validateDob(){
+        return false;
+
+    }
+    public static boolean validateJoiningDate(){
+        return false;
+    }
+    public static boolean validateSalary(){
+        return false;
+    }
 
 
 }
